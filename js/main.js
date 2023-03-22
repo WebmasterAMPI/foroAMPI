1ab9cc14ed1b643cb9ed75e1bcdf742a39fc0e41
Webmaster <webmaster@ampiguadalajara.com.mx>
2023-03-22 13:16:46 -0600

    Lo mismo pero implementando vue js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,38 @@
-var json;
+// var json;
 
-async function getInfo(){
-    let area = document.getElementById("speakers");
-    let data = await fetch('./assets.json');
-    json = await data.json();
-    console.log(json);
-    json.speakers.forEach(element => {
-            area.innerHTML += `
-            <div class="col-md-6 col-lg-2 pt-5">
-                <img data-bs-id = "${element.id}" data-bs-toggle="modal" data-bs-target="#speakersModal" class="rounded img-fluid images-speakers" src="${element.ruta}">
-                <h5 class="pt-3">${element.nombre}</h5>
-                <h7>${element.cargo}</h7>
-            </div>
-          `;
-    });
-}
+// async function getInfo(){
+//     let area = document.getElementById("speakers");
+//     let data = await fetch('./assets.json');
+//     json = await data.json();
+//     console.log(json);
+//     json.speakers.forEach(element => {
+//             area.innerHTML += `
+//             <div class="col-md-6 col-lg-2 pt-5">
+//                 <img data-bs-id = "${element.id}" data-bs-toggle="modal" data-bs-target="#speakersModal" class="rounded img-fluid images-speakers" src="${element.ruta}">
+//                 <h5 class="pt-3">${element.nombre}</h5>
+//                 <h7>${element.cargo}</h7>
+//             </div>
+//           `;
+//     });
+// }
 
-getInfo();
+// getInfo();
 
-const exampleModal = document.getElementById('speakersModal')
-        exampleModal.addEventListener('show.bs.modal', event => {
-        const button = event.relatedTarget
+// const exampleModal = document.getElementById('speakersModal')
+//         exampleModal.addEventListener('show.bs.modal', event => {
+//         const button = event.relatedTarget
   
-        const recipient = button.getAttribute('data-bs-id');
+//         const recipient = button.getAttribute('data-bs-id');
 
-        let modalNombre = exampleModal.querySelector('.modal-speaker-nombre');
-        let modalCargo = exampleModal.querySelector('.modal-speaker-cargo');
-        let modalSemblanza = exampleModal.querySelector('.modal-speaker-semblanza');
-        let modalImagen = exampleModal.querySelector('.modal-speaker-imagen');
+//         let modalNombre = exampleModal.querySelector('.modal-speaker-nombre');
+//         let modalCargo = exampleModal.querySelector('.modal-speaker-cargo');
+//         let modalSemblanza = exampleModal.querySelector('.modal-speaker-semblanza');
+//         let modalImagen = exampleModal.querySelector('.modal-speaker-imagen');
 
         
-        modalNombre.textContent = `${json.speakers[recipient-1].nombre}`;
-        modalCargo.textContent = `${json.speakers[recipient-1].cargo}`;
-        modalSemblanza.textContent = `${json.speakers[recipient-1].semblanza}`;
-        modalImagen.src = `${json.speakers[recipient-1].ruta}`;
-});
+//         modalNombre.textContent = `${json.speakers[recipient-1].nombre}`;
+//         modalCargo.textContent = `${json.speakers[recipient-1].cargo}`;
+//         modalSemblanza.textContent = `${json.speakers[recipient-1].semblanza}`;
+//         modalImagen.src = `${json.speakers[recipient-1].ruta}`;
+// });
+
